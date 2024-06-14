350de26cacff38d64d67711328332888f03542aa
nova7363 <153927952+nova7363@users.noreply.github.com>
2024-06-14 15:18:46 +0100

    script.js

--- Unit 15 html website 2/script.js
+++ Unit 15 html website 2/script.js
@@ -9,5 +9,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 15000);
+    setTimeout(showSlides, 9000);
 }
